0d6c778f78875b9e5aab94f9a43b0316e11f0577
zjubank <zjubank@gmail.com>
2017-05-29 15:44:20 +0800

    finish

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ESP8266-APRS-POST
+
+The project use `hangzhou.aprs2.net`
+
+Input format:
+```
+user passwd GPRS
+```
---
+++ main.py
@@ -0,0 +1,17 @@
+import socket
+HOST = '116.236.128.220'
+PORT = 14580
+
+while 1:
+	msg = input("Please input position:")
+	name = msg.split()[0]
+	passwd = msg.split()[1]
+	pos = msg.split()[2]
+	cmd = name+">APRS,TCPIP*:="+pos+"-ESP8266Python"
+	login = "user "+name+" pass "+passwd+" vers ESP8266Python 1.0 filter m/2000\r\n"
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((HOST, PORT))
+	s.send(login+cmd+"\r\n")
+	data = s.recv(1024)
+	print(data)
+	s.close()
